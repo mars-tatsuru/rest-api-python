d8f2b9d95edc420fb9e5394658b50b73d7d1a34a
小池　建 <t-koike@marsflag.com>
2023-04-18 16:21:49 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask,request
+
+
+app = Flask(__name__)
+
+
+stores = [
+    {
+        "name": "MyStore",
+        "items": [
+            {
+              "name": "chair",
+              "price": 15.99
+            },
+            {
+              "name": "chair",
+              "price": 15.99
+            },
+            {
+              "name": "chair",
+              "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_store():
+    return {"store": stores}
+
+@app.post("/store")
+def post_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post("/store/<string:name>/items")
+def post_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "store not found"}, 404
+
+@app.get("/store/<string:name>")
+def get_store_dynamic(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "store not found"}, 404
+
+
+@app.get("/store/<string:name>/items")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "store not found"}, 404
+
+
